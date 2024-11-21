da287eac11dbfc172eef37b7a4fd15a69e6082c4
ilyass <ilyassezzaouya4@gmail.com>
2024-11-21 03:52:55 +0100

    style modif

--- src/TexttoImage.jsx
+++ src/TexttoImage.jsx
@@ -87,13 +87,14 @@ function TextToImage() {
               </svg>
             </span>
           </div>
-          <button
-            type="submit"
-            disabled={isLoading}
-            className="w-full py-3 text-lg font-semibold text-white bg-gradient-to-r from-blue-500 to-green-500 rounded-lg hover:from-blue-600 hover:to-green-600 focus:outline-none focus:ring-4 focus:ring-blue-400 disabled:bg-gray-400"
-          >
-            {isLoading ? "Generating..." : "Generate Image"}
-          </button>
+          
+<button class="GenBtn" type="submit" disabled={isLoading}>
+    <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1" class="sparkle">
+        <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
+    </svg>
+
+    <span class="text">{isLoading ? "Generating..." : "Generate Image"}</span>
+</button>
         </form>
         {isLoading && (
           <div className="flex justify-center mt-6">
